c8ddd74b453a538d0f4e3f8ad674b2760bf6fa4f
Jonathan Leijendekker <jonathan.leijendekker@novare.com.hk>
2021-03-02 09:04:46 +0800

    optimized imports

--- src/main/java/com/leijendary/spring/microservicetemplate/error/AccessDeniedExceptionHandler.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/error/AccessDeniedExceptionHandler.java
@@ -6,8 +6,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
