d425d81b36c37e400630684a6258ba098cb3b2fe
Jonathan Leijendekker <jonathan.leijendekker@novare.com.hk>
2021-03-12 23:07:15 +0800

    Added RetryConfiguration to enable @Retryable annotations

---
+++ src/main/java/com/leijendary/spring/microservicetemplate/config/RetryConfiguration.java
@@ -0,0 +1,9 @@
+package com.leijendary.spring.microservicetemplate.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryConfiguration {
+}
