2294406143290c1e4e1ef6c43ecbf24bb959f384
Jonathan Leijendekker <jonathan.leijendekker@novare.com.hk>
2021-03-25 23:10:55 +0800

    added ZoneId

--- src/main/java/com/leijendary/spring/microservicetemplate/util/RequestContextUtil.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/util/RequestContextUtil.java
@@ -1,23 +1,34 @@
 package com.leijendary.spring.microservicetemplate.util;
 
 import org.springframework.util.Assert;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+import static java.util.Optional.ofNullable;
+import static java.util.TimeZone.getDefault;
+import static org.springframework.web.context.request.RequestContextHolder.getRequestAttributes;
+import static org.springframework.web.servlet.support.RequestContextUtils.getTimeZone;
 
 public class RequestContextUtil {
+
+    public static HttpServletRequest getCurrentRequest() {
+        final var attributes = getRequestAttributes();
+
+        Assert.state(attributes instanceof ServletRequestAttributes, "No current ServletRequestAttributes");
+
+        return ((ServletRequestAttributes) attributes).getRequest();
+    }
 
     public static String getPath() {
         return getCurrentRequest().getRequestURI();
     }
 
-    public static HttpServletRequest getCurrentRequest() {
-        RequestAttributes attributes = RequestContextHolder.getRequestAttributes();
-
-        Assert.state(attributes instanceof ServletRequestAttributes, "No current ServletRequestAttributes");
-
-        return ((ServletRequestAttributes) attributes).getRequest();
+    public static ZoneId getZoneId() {
+        return ofNullable(getTimeZone(getCurrentRequest()))
+                .map(TimeZone::toZoneId)
+                .orElse(getDefault().toZoneId());
     }
 }
