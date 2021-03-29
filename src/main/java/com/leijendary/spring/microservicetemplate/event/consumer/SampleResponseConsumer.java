226449464b479ae30b96cb20083ace28779016e6
Jonathan Leijendekker <jonathan.leijendekker@novare.com.hk>
2021-03-29 23:21:49 +0800

    fixed consumer cache error

--- src/main/java/com/leijendary/spring/microservicetemplate/event/consumer/SampleResponseConsumer.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/event/consumer/SampleResponseConsumer.java
@@ -1,7 +1,5 @@
 package com.leijendary.spring.microservicetemplate.event.consumer;
 
-import com.leijendary.spring.microservicetemplate.cache.SampleResponseCache;
-import com.leijendary.spring.microservicetemplate.cache.SampleResponsePageCache;
 import com.leijendary.spring.microservicetemplate.data.response.SampleResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,55 +9,23 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
 
-import static java.lang.Integer.parseInt;
-
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class SampleResponseConsumer {
 
-    private final SampleResponseCache sampleResponseCache;
-    private final SampleResponsePageCache sampleResponsePageCache;
-
     @Bean
     public Consumer<KStream<String, SampleResponse>> sampleResponseCreated1() {
-        return stream -> stream.foreach((key, value) -> {
-            invalidatePageCache();
-            setCache(parseInt(key), value);
-
-            log.info("Created: '{}', '{}'", key, value);
-        });
+        return stream -> stream.foreach((key, value) -> log.info("Created: '{}', '{}'", key, value));
     }
 
     @Bean
     public Consumer<KStream<String, SampleResponse>> sampleResponseUpdated1() {
-        return stream -> stream.foreach((key, value) -> {
-            invalidatePageCache();
-            setCache(parseInt(key), value);
-
-            log.info("Updated: '{}', '{}'", key, value);
-        });
+        return stream -> stream.foreach((key, value) -> log.info("Updated: '{}', '{}'", key, value));
     }
 
     @Bean
     public Consumer<KStream<String, SampleResponse>> sampleResponseDeleted1() {
-        return stream -> stream.foreach((key, value) -> {
-            invalidatePageCache();
-            deleteCache(parseInt(key));
-
-            log.info("Deleted: '{}', '{}'", key, value);
-        });
-    }
-
-    private void invalidatePageCache() {
-        sampleResponsePageCache.clear();
-    }
-
-    private void setCache(int id, SampleResponse sampleResponse) {
-        sampleResponseCache.put(id, sampleResponse);
-    }
-
-    private void deleteCache(int id) {
-        sampleResponseCache.evict(id);
+        return stream -> stream.foreach((key, value) -> log.info("Deleted: '{}', '{}'", key, value));
     }
 }
