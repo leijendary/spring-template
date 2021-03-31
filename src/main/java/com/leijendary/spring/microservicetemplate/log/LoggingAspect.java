a986d111e9d266aaff8086c504dd101d421f927d
Jonathan Leijendekker <jonathan.leijendekker@novare.com.hk>
2021-03-31 20:01:06 +0800

    added option to disable execution timer

--- src/main/java/com/leijendary/spring/microservicetemplate/log/LoggingAspect.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/log/LoggingAspect.java
@@ -1,7 +1,6 @@
 package com.leijendary.spring.microservicetemplate.log;
 
 import com.leijendary.spring.microservicetemplate.config.properties.AspectProperties;
-import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -13,18 +12,36 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
+import java.util.function.Supplier;
+
 @Component
 @ConditionalOnExpression("${aspect.logging.enabled}")
 @Aspect
-@RequiredArgsConstructor
 public class LoggingAspect {
 
     public static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    private final AspectProperties aspectProperties;
+    private final long time;
+
+    public LoggingAspect(final AspectProperties aspectProperties) {
+        this.time = aspectProperties.getExecution().getTime();
+    }
 
     @Around("execution(* com.leijendary.spring.microservicetemplate..*.*(..)) && !excludedPackages()")
-    public Object methodTimeLogger(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    public Object methodTimeLogger(final ProceedingJoinPoint proceedingJoinPoint) {
+        final var proceed = (Supplier<Object>) () -> {
+            try {
+                return proceedingJoinPoint.proceed();
+            } catch (Throwable throwable) {
+                throw new RuntimeException(throwable);
+            }
+        };
+
+        // Set aspect.execution.time to -1 to disable execution timer
+        if (time < 0) {
+            return proceed.get();
+        }
+
         final var methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
         // Get intercepted method details
         final var className = methodSignature.getDeclaringType().getSimpleName();
@@ -33,16 +50,15 @@ public class LoggingAspect {
         final var stopWatch = new StopWatch(className + "#" + methodName);
         stopWatch.start(methodName);
 
-        final var result = proceedingJoinPoint.proceed();
+        final var result = proceed.get();
 
         stopWatch.stop();
 
-        final var timeResult = stopWatch.getTotalTimeMillis();
-        final var time = aspectProperties.getExecution().getTime();
+        final var resultTime = stopWatch.getTotalTimeMillis();
 
         // Log method execution time
-        if (logger.isWarnEnabled() && timeResult >= time) {
-            final var executionTime = String.format("%s running time :: %sms", stopWatch.getId(), timeResult);
+        if (logger.isWarnEnabled() && resultTime >= time) {
+            final var executionTime = String.format("%s running time :: %sms", stopWatch.getId(), resultTime);
 
             logger.warn(executionTime);
         }
