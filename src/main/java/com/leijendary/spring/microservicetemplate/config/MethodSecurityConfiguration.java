b26ee9ea42d41e36aa6e77f8bf08010b1e5f2109
Jonathan Leijendekker <jonathan.leijendekker@novare.com.hk>
2021-04-02 15:23:54 +0800

    settled down in using hasAuthority with SCOPE_ prefix

--- src/main/java/com/leijendary/spring/microservicetemplate/config/MethodSecurityConfiguration.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/config/MethodSecurityConfiguration.java
@@ -1,14 +1,8 @@
 package com.leijendary.spring.microservicetemplate.config;
 
-import com.leijendary.spring.microservicetemplate.security.ScopeMethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
-
-    @Override
-    protected ScopeMethodSecurityExpressionHandler createExpressionHandler() {
-        return new ScopeMethodSecurityExpressionHandler();
-    }
 }
--- src/main/java/com/leijendary/spring/microservicetemplate/error/AccessDeniedExceptionHandler.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/error/AccessDeniedExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.leijendary.spring.microservicetemplate.error;
 
 import com.leijendary.spring.microservicetemplate.data.response.AccessDeniedResponse;
-import com.leijendary.spring.microservicetemplate.data.response.InsufficientScopeResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.oauth2.common.exceptions.InsufficientScopeException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -25,17 +23,7 @@ public class AccessDeniedExceptionHandler {
     public ResponseEntity<AccessDeniedResponse> catchAccessDenied(final AccessDeniedException exception) {
         final var message = messageSource.getMessage("access.denied", new Object[0], getDefault());
         final var error = messageSource.getMessage("access.error", new Object[0], getDefault());
-        var response = new AccessDeniedResponse(error, message + ": " + exception.getMessage());
-
-        // For Insufficient Scope errors
-        if (exception.getCause() instanceof InsufficientScopeException) {
-            final var cause = (InsufficientScopeException) exception.getCause();
-            final var scopes = cause.getAdditionalInformation()
-                    .get("scope")
-                    .split("\\s+");
-
-            response = new InsufficientScopeResponse(error, exception.getMessage(), scopes);
-        }
+        final var response = new AccessDeniedResponse(error, message + ": " + exception.getMessage());
 
         return status(response.getStatus()).body(response);
     }
