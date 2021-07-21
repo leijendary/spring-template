356ba5ed096b5663bf0084f39804ced26a5b0c73
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-07-21 17:59:01 +0800

    removed auditable model

--- src/main/java/com/leijendary/spring/microservicetemplate/model/SampleTable.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/model/SampleTable.java
@@ -3,20 +3,38 @@ package com.leijendary.spring.microservicetemplate.model;
 import com.leijendary.spring.microservicetemplate.model.listener.SampleTableListener;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import java.time.Instant;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Entity
-@EntityListeners(SampleTableListener.class)
-public class SampleTable extends AuditableModel {
+@EntityListeners({ AuditingEntityListener.class, SampleTableListener.class })
+public class SampleTable extends SnowflakeIdModel {
 
     @Column(name = "column_1")
     private String column1;
 
     @Column(name = "column_2")
     private int column2;
+
+    @CreatedDate
+    private Instant createdDate;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private Instant lastModifiedDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
 }
