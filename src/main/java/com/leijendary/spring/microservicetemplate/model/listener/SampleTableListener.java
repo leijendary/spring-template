e62493b989ccb6e2ddf9786e666252532d855c8e
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-07-21 22:43:56 +0800

    used getBean instead of inject

--- src/main/java/com/leijendary/spring/microservicetemplate/model/listener/SampleTableListener.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/model/listener/SampleTableListener.java
@@ -3,20 +3,18 @@ package com.leijendary.spring.microservicetemplate.model.listener;
 import com.leijendary.spring.microservicetemplate.event.producer.SampleProducer;
 import com.leijendary.spring.microservicetemplate.model.SampleTable;
 
-import javax.inject.Inject;
 import javax.persistence.PostPersist;
 import javax.persistence.PostRemove;
 import javax.persistence.PostUpdate;
 
 import static com.leijendary.spring.microservicetemplate.factory.SampleFactory.toSchema;
+import static com.leijendary.spring.microservicetemplate.util.SpringContext.getBean;
 
 public class SampleTableListener {
 
-    @Inject
-    private SampleProducer sampleProducer;
-
     @PostPersist
     public void onSave(final SampleTable sampleTable) {
+        final var sampleProducer = getBean(SampleProducer.class);
         final var sampleSchema = toSchema(sampleTable);
 
         sampleProducer.created(sampleSchema);
@@ -24,6 +22,7 @@ public class SampleTableListener {
 
     @PostUpdate
     public void onUpdate(final SampleTable sampleTable) {
+        final var sampleProducer = getBean(SampleProducer.class);
         final var sampleSchema = toSchema(sampleTable);
 
         sampleProducer.updated(sampleSchema);
@@ -31,6 +30,7 @@ public class SampleTableListener {
 
     @PostRemove
     public void onDelete(final SampleTable sampleTable) {
+        final var sampleProducer = getBean(SampleProducer.class);
         final var sampleSchema = toSchema(sampleTable);
 
         sampleProducer.deleted(sampleSchema);
