9d09d5137d24ccdbfeb937f48dc54314ca0f6b53
Jonathan Leijendekker <jonathan.leijendekker@novare.com.hk>
2021-07-27 15:39:48 +0800

    added final

--- src/main/java/com/leijendary/spring/microservicetemplate/validator/v1/SampleRequestV1FieldsNotSameV1Validator.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/validator/v1/SampleRequestV1FieldsNotSameV1Validator.java
@@ -11,12 +11,12 @@ import static java.util.Optional.ofNullable;
 public class SampleRequestV1FieldsNotSameV1Validator implements ConstraintValidator<FieldsNotEqualV1, SampleRequestV1> {
 
     @Override
-    public void initialize(FieldsNotEqualV1 constraintAnnotation) {
+    public void initialize(final FieldsNotEqualV1 constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
     @Override
-    public boolean isValid(SampleRequestV1 value, ConstraintValidatorContext context) {
+    public boolean isValid(final SampleRequestV1 value, final ConstraintValidatorContext context) {
         return ofNullable(value.getField1())
                 .map(field1 -> !field1.equals(String.valueOf(value.getField2())))
                 .orElse(true);
