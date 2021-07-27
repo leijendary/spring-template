29e584576c4821da38b402cbcbef654cc92ae41a
Jonathan Leijendekker <jonathan.leijendekker@novare.com.hk>
2021-07-27 12:00:54 +0800

    Removed @AllArgsConstructor from QueryRequest

--- src/main/java/com/leijendary/spring/microservicetemplate/data/request/QueryRequest.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/data/request/QueryRequest.java
@@ -1,10 +1,8 @@
 package com.leijendary.spring.microservicetemplate.data.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class QueryRequest {
 
     private String query;
