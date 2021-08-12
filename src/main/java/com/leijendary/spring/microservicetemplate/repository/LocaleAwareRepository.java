4b061ffcc22a35e1ee637a4c59757e2c8aec5e68
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-08-12 15:30:07 +0800

    WIP: implementation

--- src/main/java/com/leijendary/spring/microservicetemplate/repository/LocaleAwareRepository.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/repository/LocaleAwareRepository.java
@@ -1,7 +1,28 @@
 package com.leijendary.spring.microservicetemplate.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import com.leijendary.spring.microservicetemplate.model.LocaleModel;
+import com.leijendary.spring.microservicetemplate.model.LocalizedModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.lang.Nullable;
 
-public interface LocaleAwareRepository<T, ID> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
+import java.util.List;
+import java.util.Optional;
+
+@NoRepositoryBean
+public interface LocaleAwareRepository<R extends LocalizedModel<R, T>, T extends LocaleModel<R>, ID> {
+
+    Optional<R> findById(final ID id);
+
+    Page<R> findAll(@Nullable Specification<R> specification, Pageable pageable);
+
+    List<R> findAll();
+
+    R save(final R entity);
+
+    void delete(final R entity);
+
+    long count();
 }
