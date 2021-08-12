a75edda1cc09c71766046d9b89fbf7f93a1f29ac
Jonathan Leijendekker <8501171+leijendary@users.noreply.github.com>
2021-08-12 15:35:07 +0800

    Update SampleTableRepository.java

--- src/main/java/com/leijendary/spring/microservicetemplate/repository/SampleTableRepository.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/repository/SampleTableRepository.java
@@ -1,12 +1,11 @@
 package com.leijendary.spring.microservicetemplate.repository;
 
 import com.leijendary.spring.microservicetemplate.model.SampleTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import com.leijendary.spring.microservicetemplate.model.SampleTableTranslation;
 
 import java.util.Optional;
 
-public interface SampleTableRepository extends JpaRepository<SampleTable, Long>, JpaSpecificationExecutor<SampleTable> {
+public interface SampleTableRepository extends LocaleAwareRepository<SampleTable, SampleTableTranslation, Long> {
 
     Optional<SampleTable> findFirstByColumn1IgnoreCaseAndIdNot(final String column1, final long id);
 }
