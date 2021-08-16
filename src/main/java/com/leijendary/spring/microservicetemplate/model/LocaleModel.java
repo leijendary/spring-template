f356ddebd7783deb69a2512cc0041101786716e2
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-08-16 21:44:39 +0800

    added audit in LocaleModel

--- src/main/java/com/leijendary/spring/microservicetemplate/model/LocaleModel.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/model/LocaleModel.java
@@ -2,14 +2,22 @@ package com.leijendary.spring.microservicetemplate.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
+import java.time.OffsetDateTime;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class LocaleModel<R> extends IdentityIdModel {
 
     @ManyToOne
@@ -18,4 +26,16 @@ public abstract class LocaleModel<R> extends IdentityIdModel {
 
     private String language;
     private int ordinal;
+
+    @CreatedDate
+    private OffsetDateTime createdDate;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private OffsetDateTime lastModifiedDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
 }
