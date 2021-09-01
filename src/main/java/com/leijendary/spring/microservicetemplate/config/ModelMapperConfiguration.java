b9a62e7360ebf053b2b5a710c7dcf2ddf4e07b65
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-09-01 20:01:16 +0800

    added property condition

--- src/main/java/com/leijendary/spring/microservicetemplate/config/ModelMapperConfiguration.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/config/ModelMapperConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+import static org.modelmapper.Conditions.isNotNull;
 import static org.modelmapper.convention.MatchingStrategies.STRICT;
 import static org.modelmapper.module.jsr310.Jsr310ModuleConfig.builder;
 
@@ -22,6 +23,8 @@ public class ModelMapperConfiguration {
         modelMapper.registerModule(new Jsr310Module(config));
         // Matching strategy set to "STRICT" to avoid mismatching of fields
         modelMapper.getConfiguration().setMatchingStrategy(STRICT);
+        // Skip if the property is already null
+        modelMapper.getConfiguration().setPropertyCondition(isNotNull());
         // SampleRequestV1 to SampleData mapping
         modelMapper.addMappings(new SampleRequestV1ToSampleDataMap());
 
