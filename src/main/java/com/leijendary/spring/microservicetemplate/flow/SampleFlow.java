66290420c7697d7edaeefe129b1b01bfd5252524
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-10-05 20:41:04 +0800

    for some reason, transactions are fixed???

--- src/main/java/com/leijendary/spring/microservicetemplate/flow/SampleFlow.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/flow/SampleFlow.java
@@ -11,8 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.leijendary.spring.microservicetemplate.factory.SampleFactory.toResponseV1;
 
