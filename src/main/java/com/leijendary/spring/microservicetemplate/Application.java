efbc553d341a8880b8647606081237716adefdde
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-10-07 15:58:55 +0800

    added NoHandlerFoundExceptionHandler for 404 mappings

--- src/main/java/com/leijendary/spring/microservicetemplate/Application.java
+++ src/main/java/com/leijendary/spring/microservicetemplate/Application.java
@@ -1,19 +1,21 @@
 package com.leijendary.spring.microservicetemplate;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import sun.misc.Unsafe;
+
+import static java.lang.System.setProperty;
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         // Disable devtools restart for avro
-        System.setProperty("spring.devtools.restart.enabled", "false");
+        setProperty("spring.devtools.restart.enabled", "false");
 
         disableIllegalAccessLoggerWarning();
 
-        SpringApplication.run(Application.class, args);
+        run(Application.class, args);
     }
 
     public static void disableIllegalAccessLoggerWarning() {
