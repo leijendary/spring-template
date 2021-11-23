cce4a1ba6aaa9bd1b6fffc4aa325791382247349
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-11-23 18:24:19 +0800

    static import

--- src/test/java/com/leijendary/spring/boot/template/ApplicationTests.java
+++ src/test/java/com/leijendary/spring/boot/template/ApplicationTests.java
@@ -1,9 +1,11 @@
 package com.leijendary.spring.boot.template;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public abstract class ApplicationTests {
 
     @Test
