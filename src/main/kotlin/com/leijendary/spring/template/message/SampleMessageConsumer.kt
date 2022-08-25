a22fad802a34e1cbc702fe810b257639eb674eda
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2022-08-25 20:12:25 +0200

    used KStream again

--- src/main/kotlin/com/leijendary/spring/template/message/SampleMessageConsumer.kt
+++ src/main/kotlin/com/leijendary/spring/template/message/SampleMessageConsumer.kt
@@ -1,24 +1,25 @@
 package com.leijendary.spring.template.message
 
 import com.leijendary.spring.template.api.v1.data.SampleMessage
+import org.apache.kafka.streams.kstream.KStream
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 import java.util.function.Consumer
 
-@Configuration
+@Component
 class SampleMessageConsumer {
     @Bean
-    fun sampleCreated(): Consumer<SampleMessage> {
+    fun sampleCreated(): Consumer<KStream<String, SampleMessage>> {
         return Consumer { }
     }
 
     @Bean
-    fun sampleUpdated(): Consumer<SampleMessage> {
+    fun sampleUpdated(): Consumer<KStream<String, SampleMessage>> {
         return Consumer { }
     }
 
     @Bean
-    fun sampleDeleted(): Consumer<SampleMessage> {
+    fun sampleDeleted(): Consumer<KStream<String, SampleMessage>> {
         return Consumer { }
     }
 }
