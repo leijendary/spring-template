71eb010c27649e7568dc8240cea3a69a7cce76b6
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2022-08-25 22:19:51 +0200

    Moved @Enable* annotations without configuration to Application.kt

--- src/main/kotlin/com/leijendary/spring/template/Application.kt
+++ src/main/kotlin/com/leijendary/spring/template/Application.kt
@@ -2,8 +2,20 @@ package com.leijendary.spring.template
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.retry.annotation.EnableRetry
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableAsync
+@EnableCaching
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableKafka
+@EnableRetry
 class Application
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/leijendary/spring/template/core/config/CacheConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package com.leijendary.spring.template.core.config
-
-import org.springframework.cache.annotation.EnableCaching
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@EnableCaching
-class CacheConfiguration 
