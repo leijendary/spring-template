476810cca99c9bdc8a7735271e34f535f2ddb2f0
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2022-08-25 22:19:51 +0200

    Moved Enable* annotations without configuration to Application.kt

--- src/main/kotlin/com/leijendary/spring/template/core/config/KafkaConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package com.leijendary.spring.template.core.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.annotation.EnableKafka
-
-@Configuration
-@EnableKafka
-class KafkaConfiguration 
