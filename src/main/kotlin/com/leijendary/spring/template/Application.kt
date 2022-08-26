99f523c4209d3722e8c2a51fbf48a345542047df
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2022-08-26 21:45:53 +0200

    moved slow SQL to profile and excluded class to Application.kt

--- src/main/kotlin/com/leijendary/spring/template/Application.kt
+++ src/main/kotlin/com/leijendary/spring/template/Application.kt
@@ -1,6 +1,8 @@
 package com.leijendary.spring.template
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
@@ -9,13 +11,18 @@ import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.retry.annotation.EnableRetry
 import org.springframework.scheduling.annotation.EnableAsync
 
-@SpringBootApplication
 @EnableAsync
 @EnableCaching
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableKafka
 @EnableRetry
+@SpringBootApplication(
+    exclude = [
+        ErrorMvcAutoConfiguration::class,
+        UserDetailsServiceAutoConfiguration::class,
+    ]
+)
 class Application
 
 fun main(args: Array<String>) {
