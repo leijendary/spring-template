94e397640bacee76edfa770aad65ce96e73c01f6
Jonathan Leijendekker <jonathan.leijendekker@nl.abnamro.com>
2023-01-19 09:24:13 +0100

    seek now has all the fields for the response

--- src/main/kotlin/com/leijendary/spring/template/core/data/Seek.kt
+++ src/main/kotlin/com/leijendary/spring/template/core/data/Seek.kt
@@ -1,6 +1,10 @@
 package com.leijendary.spring.template.core.data
 
-class Seek<T>(val content: List<T>, val nextToken: String?, val size: Int, val seekable: Seekable) {
+class Seek<T>(val content: List<T>, val nextToken: String?, val size: Int, private val seekable: Seekable) {
+    val limit: Int = seek.seekable.limit
+    val sortProperty: String = seek.seekable.sort.map { it.property }.joinToString(",")
+    val sortDirection: String = seek.seekable.direction.name.lowercase()
+    
     fun <R> map(transform: (T) -> R): Seek<R> {
         val content = content.map(transform)
 
