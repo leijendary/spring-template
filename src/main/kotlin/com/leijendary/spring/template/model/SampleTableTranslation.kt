950c84d35618a66af90a430664670aea159b60d1
Jonathan Leijendekker <jonathan.leijendekker@nl.abnamro.com>
2023-01-19 15:23:04 +0100

    changed new lines

--- src/main/kotlin/com/leijendary/spring/template/model/SampleTableTranslation.kt
+++ src/main/kotlin/com/leijendary/spring/template/model/SampleTableTranslation.kt
@@ -5,5 +5,8 @@ import jakarta.persistence.Embeddable
 
 @Embeddable
 data class SampleTableTranslation(
-    var name: String, var description: String, override var language: String, override var ordinal: Int
+    var name: String,
+    var description: String,
+    override var language: String,
+    override var ordinal: Int
 ) : LocaleModel(language, ordinal)
