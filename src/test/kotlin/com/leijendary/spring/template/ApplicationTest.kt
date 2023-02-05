41cc3b0416aceedd036aee6ff644415a778c832a
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2023-02-05 11:51:10 +0100

    Removed @ExtendWith in test

--- src/test/kotlin/com/leijendary/spring/template/ApplicationTest.kt
+++ src/test/kotlin/com/leijendary/spring/template/ApplicationTest.kt
@@ -1,17 +1,14 @@
 package com.leijendary.spring.template
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.domain.Sort
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MockMvcResultMatchersDsl
 
 @SpringBootTest
-@ExtendWith(SpringExtension::class)
 @AutoConfigureMockMvc
 class ApplicationTest {
     @Autowired
