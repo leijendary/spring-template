1330a1bc79db7b6af88e9b2041dbb60d683554bc
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2023-02-12 00:21:53 +0100

    used auditorAware

--- src/main/kotlin/com/leijendary/spring/template/core/repository/SoftDeleteRepositoryImpl.kt
+++ src/main/kotlin/com/leijendary/spring/template/core/repository/SoftDeleteRepositoryImpl.kt
@@ -2,20 +2,22 @@ package com.leijendary.spring.template.core.repository
 
 import com.leijendary.spring.template.core.entity.SoftDeleteEntity
 import com.leijendary.spring.template.core.util.RequestContext.now
-import com.leijendary.spring.template.core.util.RequestContext.userId
 import jakarta.persistence.EntityManager
 import jakarta.persistence.PersistenceContext
+import org.springframework.data.domain.AuditorAware
 import org.springframework.stereotype.Repository
 
 @Repository
 class SoftDeleteRepositoryImpl<T : SoftDeleteEntity>(
+    private val auditorAware: AuditorAware<String>,
+
     @PersistenceContext
     private val entityManager: EntityManager
 ) : SoftDeleteRepository<T> {
 
     override fun softDelete(entity: T) {
         entity.deletedAt = now
-        entity.deletedBy = userId
+        entity.deletedBy = auditorAware.currentAuditor.get()
 
         entityManager.persist(entity)
     }
