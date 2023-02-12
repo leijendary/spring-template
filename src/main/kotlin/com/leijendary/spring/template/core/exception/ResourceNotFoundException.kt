969b4a51e4115c7caff81f2b83bb010740e2e1da
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2023-02-12 19:32:47 +0100

    removed exception message that is not needed

--- src/main/kotlin/com/leijendary/spring/template/core/exception/ResourceNotFoundException.kt
+++ src/main/kotlin/com/leijendary/spring/template/core/exception/ResourceNotFoundException.kt
@@ -1,5 +1,3 @@
 package com.leijendary.spring.template.core.exception
 
-class ResourceNotFoundException(val source: List<Any>, val identifier: Any) : RuntimeException(
-    String.format("%s with identifier %s not found", source, identifier)
-)
+class ResourceNotFoundException(val source: List<Any>, val identifier: Any) : RuntimeException()
