e9a580e9db5e83569d7f717b193dd95ce3b50832
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2023-02-15 00:09:00 +0100

    removed whitelisted fields to IAM template

--- src/main/kotlin/com/leijendary/spring/template/core/json/StringTrimDeserializer.kt
+++ src/main/kotlin/com/leijendary/spring/template/core/json/StringTrimDeserializer.kt
@@ -5,18 +5,11 @@ import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.deser.std.StringDeserializer
 import org.springframework.boot.jackson.JsonComponent
 
-private val exclusions = arrayOf("currentPassword", "password")
-
 @JsonComponent
 class StringTrimDeserializer : StringDeserializer() {
     override fun deserialize(jsonParser: JsonParser, deserializationContext: DeserializationContext): String? {
         // Get the result from the original StringDeserializer
         val deserialized = super.deserialize(jsonParser, deserializationContext)
-
-        // But skip the whitelisted fields
-        if (exclusions.contains(jsonParser.currentName())) {
-            return deserialized
-        }
 
         // ... and just trim it
         return deserialized?.let { obj -> obj.trim { it <= ' ' } }
