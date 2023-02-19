04eb13138c41b0abb362f93166b04f34ac0d6269
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2023-02-19 22:50:39 +0100

    added presigned URL generator

--- src/main/kotlin/com/leijendary/spring/template/core/storage/S3Storage.kt
+++ src/main/kotlin/com/leijendary/spring/template/core/storage/S3Storage.kt
@@ -7,11 +7,62 @@ import software.amazon.awssdk.core.ResponseInputStream
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.*
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest
 import java.io.File
 
 @Service
-class S3Storage(awsS3Properties: AwsS3Properties, private val s3Client: S3Client) {
+class S3Storage(
+    awsS3Properties: AwsS3Properties,
+    private val s3Client: S3Client,
+    private val s3Presigner: S3Presigner
+) {
     private val bucketName = awsS3Properties.bucketName
+
+    enum class Request {
+        GET,
+        PUT
+    }
+
+    fun sign(key: String, request: Request = Request.GET) = when (request) {
+        Request.GET -> signGet(key)
+        Request.PUT -> signPut(key)
+    }
+
+    fun signGet(key: String): String {
+        val request = GetObjectRequest
+            .builder()
+            .bucket(bucketName)
+            .key(key)
+            .build()
+        val signRequest = GetObjectPresignRequest
+            .builder()
+            .getObjectRequest(request)
+            .build()
+
+        return s3Presigner
+            .presignGetObject(signRequest)
+            .url()
+            .toString()
+    }
+
+    fun signPut(key: String): String {
+        val request = PutObjectRequest
+            .builder()
+            .bucket(bucketName)
+            .key(key)
+            .build()
+        val signRequest = PutObjectPresignRequest
+            .builder()
+            .putObjectRequest(request)
+            .build()
+
+        return s3Presigner
+            .presignPutObject(signRequest)
+            .url()
+            .toString()
+    }
 
     fun get(key: String): GetObjectResponse {
         val s3Object = stream(key)
