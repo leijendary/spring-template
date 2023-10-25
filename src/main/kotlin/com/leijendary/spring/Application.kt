2fb98ba8046c174dd91c33a01421d9ea90ce1db9
leijendary <jonathanleijendekker@gmail.com>
2023-10-25 17:39:21 +0200

    Used aws for now

--- src/main/kotlin/com/leijendary/spring/Application.kt
+++ src/main/kotlin/com/leijendary/spring/Application.kt
@@ -2,7 +2,13 @@ package com.leijendary.spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.retry.annotation.EnableRetry
 
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableRetry
 @SpringBootApplication
 class Application
 
