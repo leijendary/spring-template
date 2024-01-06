0ec465f39716f706294f1a7005118a53c6e3e446
leijendary <jonathanleijendekker@gmail.com>
2024-01-06 20:32:23 +0100

    Fixed @Transaction(readOnly = true) not working

--- src/main/kotlin/com/leijendary/config/DatabaseConfiguration.kt
+++ src/main/kotlin/com/leijendary/config/DatabaseConfiguration.kt
@@ -1,22 +1,31 @@
 package com.leijendary.config
 
+import com.leijendary.config.DataSourceType.READ_ONLY
+import com.leijendary.config.DataSourceType.READ_WRITE
 import com.leijendary.config.properties.DataSourcePrimaryProperties
 import com.leijendary.config.properties.DataSourceReadOnlyProperties
 import com.zaxxer.hikari.HikariDataSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
-import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource
+import org.springframework.jdbc.support.JdbcTransactionManager
+import org.springframework.stereotype.Component
+import org.springframework.transaction.TransactionDefinition
+import org.springframework.transaction.support.TransactionSynchronizationManager.*
 import javax.sql.DataSource
+
+enum class DataSourceType {
+    READ_WRITE,
+    READ_ONLY
+}
 
 @Configuration
 class DatabaseConfiguration {
     @Bean
     @Primary
     fun dataSource(primaryDataSource: DataSource, readOnlyDataSource: DataSource): DataSource {
-        return LazyConnectionDataSourceProxy(primaryDataSource).apply {
-            setReadOnlyDataSource(readOnlyDataSource)
-        }
+        return TransactionRoutingDataSource(primaryDataSource, readOnlyDataSource)
     }
 
     @Bean
@@ -28,4 +37,34 @@ class DatabaseConfiguration {
     fun readOnlyDataSource(config: DataSourceReadOnlyProperties): DataSource {
         return HikariDataSource(config)
     }
+}
+
+@Component
+class SynchronizedJdbcTransactionManager(dataSource: DataSource) : JdbcTransactionManager(dataSource) {
+    override fun doBegin(transaction: Any, definition: TransactionDefinition) {
+        setCurrentTransactionName(definition.name)
+        setCurrentTransactionIsolationLevel(definition.isolationLevel)
+        setCurrentTransactionReadOnly(definition.isReadOnly)
+
+        super.doBegin(transaction, definition)
+    }
+}
+
+class TransactionRoutingDataSource(primary: DataSource, readOnly: DataSource) : AbstractRoutingDataSource() {
+    init {
+        val map = mapOf<Any, Any>(
+            READ_WRITE to primary,
+            READ_ONLY to readOnly
+        )
+
+        setTargetDataSources(map)
+        setDefaultTargetDataSource(primary)
+    }
+
+    override fun determineCurrentLookupKey(): Any {
+        val isReadOnly = isCurrentTransactionReadOnly()
+
+        return if (isReadOnly) READ_ONLY else READ_WRITE
+    }
+
 }
--- src/main/kotlin/com/leijendary/util/BeanContainer.kt
+++ src/main/kotlin/com/leijendary/util/BeanContainer.kt
@@ -7,7 +7,7 @@ import com.leijendary.security.Encryption
 import com.leijendary.storage.BlockStorage
 import com.leijendary.util.SpringContext.Companion.getBean
 import io.micrometer.tracing.Tracer
-import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.jdbc.support.JdbcTransactionManager
 
 object BeanContainer {
     val authProperties by lazy { getBean(AuthProperties::class) }
@@ -16,5 +16,5 @@ object BeanContainer {
     val numberProperties by lazy { getBean(NumberProperties::class) }
     val objectMapper by lazy { getBean(ObjectMapper::class) }
     val tracer by lazy { getBean(Tracer::class) }
-    val transactionManager by lazy { getBean(PlatformTransactionManager::class) }
+    val transactionManager by lazy { getBean(JdbcTransactionManager::class) }
 }
