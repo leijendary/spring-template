a80489c4883922eecb7d2d700137b4def27b897a
leijendary <jonathanleijendekker@gmail.com>
2024-01-07 19:14:14 +0100

    Added KAFKA_HEAP_OPTS

--- src/test/kotlin/com/leijendary/container/KafkaContainerTest.kt
+++ src/test/kotlin/com/leijendary/container/KafkaContainerTest.kt
@@ -9,7 +9,9 @@ import org.testcontainers.utility.DockerImageName
 class KafkaContainerTest {
     companion object {
         private val image = DockerImageName.parse("confluentinc/cp-kafka:7.5.3")
-        private val kafka = KafkaContainer(image).withKraft()
+        private val kafka = KafkaContainer(image)
+            .withEnv("KAFKA_HEAP_OPTS", "-Xms128m -Xmx256m")
+            .withKraft()
     }
 
     internal class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
