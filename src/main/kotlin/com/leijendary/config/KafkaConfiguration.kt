e4f13fa29074bea8ccaafc310f75711bdd9ac64d
leijendary <jonathanleijendekker@gmail.com>
2024-01-14 20:36:41 +0100

    [no ci] Added ackMode to containerProperties

--- src/main/kotlin/com/leijendary/config/KafkaConfiguration.kt
+++ src/main/kotlin/com/leijendary/config/KafkaConfiguration.kt
@@ -3,6 +3,7 @@ package com.leijendary.config
 import com.leijendary.config.properties.KafkaTopicProperties
 import com.leijendary.interceptor.KafkaInterceptor
 import org.apache.kafka.common.TopicPartition
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
@@ -21,6 +22,7 @@ private const val TOPIC_DEAD_LETTER_SUFFIX = ".error"
 @EnableKafka
 class KafkaConfiguration(
     private val kafkaInterceptor: KafkaInterceptor,
+    private val kafkaProperties: KafkaProperties,
     private val kafkaTopicProperties: KafkaTopicProperties,
 ) {
     @Bean
@@ -55,6 +57,7 @@ class KafkaConfiguration(
 
         return ConcurrentKafkaListenerContainerFactory<String, String>().apply {
             this.consumerFactory = consumerFactory
+            containerProperties.ackMode = kafkaProperties.listener.ackMode
             containerProperties.isMicrometerEnabled = true
             containerProperties.isObservationEnabled = true
             setCommonErrorHandler(errorHandler)
