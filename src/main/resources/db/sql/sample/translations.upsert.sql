8adcef42957fd3a99f1b29420275d1ee987e659c
leijendary <jonathanleijendekker@gmail.com>
2024-02-22 23:39:08 +0100

    [no ci] updated do update

--- src/main/resources/db/sql/sample/translations.upsert.sql
+++ src/main/resources/db/sql/sample/translations.upsert.sql
@@ -1,7 +1,6 @@
 insert into sample_translation (id, name, description, language, ordinal)
 select * from unnest(?::int[], ?::text[], ?::text[], ?::text[], ?::smallint[])
-on conflict (id, language)
-do update
+on conflict (id, language) do update
 set
     name = excluded.name,
     description = excluded.description,
