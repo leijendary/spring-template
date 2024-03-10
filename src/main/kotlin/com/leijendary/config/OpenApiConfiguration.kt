37425832e9f2388c34c9c86f2dee073c8ac7bd54
leijendary <jonathanleijendekker@gmail.com>
2024-03-10 22:08:03 +0100

    [no ci] Added LocalDateTime openapi override

--- src/main/kotlin/com/leijendary/config/OpenApiConfiguration.kt
+++ src/main/kotlin/com/leijendary/config/OpenApiConfiguration.kt
@@ -1,9 +1,23 @@
 package com.leijendary.config
 
 import com.leijendary.config.properties.OpenApiProperties
+import io.swagger.v3.oas.models.media.Schema
+import org.springdoc.core.utils.SpringDocUtils
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME
+import java.time.temporal.ChronoUnit.SECONDS
 
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(OpenApiProperties::class)
-class OpenApiConfiguration
+class OpenApiConfiguration {
+    init {
+        val schema = Schema<LocalDateTime>()
+        schema.type = "string"
+        schema.format = "yyyyMMdd'T'HH:mm:ss"
+        schema.example = LocalDateTime.now().truncatedTo(SECONDS).format(ISO_LOCAL_DATE_TIME)
+
+        SpringDocUtils.getConfig().replaceWithSchema(LocalDateTime::class.java, schema)
+    }
+}
