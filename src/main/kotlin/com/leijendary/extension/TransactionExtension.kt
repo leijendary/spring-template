ae2276ce7c4f2ce1e7336893336dafa97b41ec86
leijendary <jonathanleijendekker@gmail.com>
2024-05-21 21:26:38 +0200

    [no ci] added afterCommit

--- src/main/kotlin/com/leijendary/extension/TransactionExtension.kt
+++ src/main/kotlin/com/leijendary/extension/TransactionExtension.kt
@@ -3,6 +3,8 @@ package com.leijendary.extension
 import com.leijendary.util.readOnlyTransactionalTemplate
 import com.leijendary.util.transactionTemplate
 import org.springframework.transaction.TransactionStatus
+import org.springframework.transaction.support.TransactionSynchronization
+import org.springframework.transaction.support.TransactionSynchronizationManager.registerSynchronization
 
 /**
  * Use this function if you don't want your whole method to run under a single transaction.
@@ -14,3 +16,12 @@ fun <T> transactional(readOnly: Boolean = false, function: (TransactionStatus) -
 
     return template.execute(function)!!
 }
+
+/**
+ * Use this function when the code is inside a transaction and you want to run something when the transaction ends.
+ */
+fun afterCommit(function: () -> Any) = registerSynchronization(object : TransactionSynchronization {
+    override fun afterCommit() {
+        function()
+    }
+})
