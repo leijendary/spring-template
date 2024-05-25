8b32e08e3b552cc209639877677b4dfa89f4a96c
leijendary <jonathanleijendekker@gmail.com>
2024-05-25 23:03:04 +0200

    [no ci] removed BigDecimalSerializer.kt and added math context for percent divide

--- src/main/kotlin/com/leijendary/extension/BigDecimalExtension.kt
+++ src/main/kotlin/com/leijendary/extension/BigDecimalExtension.kt
@@ -2,19 +2,20 @@ package com.leijendary.extension
 
 import java.math.BigDecimal
 import java.math.BigDecimal.ZERO
+import java.math.MathContext.DECIMAL32
 import java.math.RoundingMode
 import java.math.RoundingMode.HALF_UP
 
 val ONE_HUNDRED = 100.toBigDecimal()
-
-fun BigDecimal.scaled(scale: Int = 2, mode: RoundingMode = HALF_UP): BigDecimal {
-    return setScale(scale, mode)
-}
 
 fun BigDecimal.percent(value: BigDecimal): BigDecimal {
     if (this.compareTo(ZERO) == 0 || value.compareTo(ZERO) == 0) {
         return ZERO
     }
 
-    return this * value / ONE_HUNDRED
+    return (this * value).divide(ONE_HUNDRED, DECIMAL32)
+}
+
+fun BigDecimal.scaled(scale: Int = 2, mode: RoundingMode = HALF_UP): BigDecimal {
+    return setScale(scale, mode)
 }
