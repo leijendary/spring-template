4cc0f99d5ff036b13b86fa9c04902110862375be
leijendary <jonathanleijendekker@gmail.com>
2024-05-30 21:40:45 +0200

    [no ci] updated liquibase

--- src/main/resources/db/changelog/db.changelog-0.0.1.sql
+++ src/main/resources/db/changelog/db.changelog-0.0.1.sql
@@ -1,31 +1,37 @@
 --liquibase formatted sql
 --changeset leijendary:create-sample-table
-CREATE TABLE sample (
-    id bigint GENERATED ALWAYS AS IDENTITY (START 100000) PRIMARY KEY,
-    name character varying(100) NOT NULL,
-    description text,
-    amount numeric(12,2) NOT NULL,
-    version int NOT NULL DEFAULT 0,
-    created_at timestamp with time zone NOT NULL DEFAULT now(),
-    created_by text NOT NULL,
-    last_modified_at timestamp with time zone NOT NULL DEFAULT now(),
-    last_modified_by text NOT NULL,
+create table sample (
+    id bigint generated always as identity primary key,
+    name character varying(100) not null,
+    description character varying(2000),
+    amount numeric(12,2) not null,
+    version smallint not null default 0,
+    created_at timestamp with time zone not null default now(),
+    created_by text not null,
+    last_modified_at timestamp with time zone not null default now(),
+    last_modified_by text not null,
     deleted_at timestamp with time zone,
     deleted_by text
 );
 
+--changeset leijendary:set-sample-id-initial-value
+select setval('sample_id_seq', (select floor(random() * 19999 + 110000)::bigint));
+
 --changeset leijendary:create-sample-name-lower-unique-index
-CREATE UNIQUE INDEX sample_name_key ON sample(lower(name)) WHERE deleted_at IS NULL;
+create unique index sample_name_key on sample(lower(name)) where deleted_at is null;
 
 --changeset leijendary:create-sample-created-at-id-index
-CREATE INDEX sample_created_at_id_idx ON sample(created_at DESC, id DESC);
+create index sample_created_at_id_idx on sample(created_at desc, id desc);
 
 --changeset leijendary:create-sample-translation-table
-CREATE TABLE sample_translation (
-    id bigint REFERENCES sample(id),
-    name character varying(100) NOT NULL,
+create table sample_translation (
+    id bigint references sample(id),
+    name character varying(100) not null,
     description character varying(200),
     language character varying(4),
-    ordinal smallint NOT NULL,
-    PRIMARY KEY (id, language)
+    ordinal smallint not null,
+    constraint sample_translation_pkey primary key (id, language)
 );
+
+--changeset leijendary:create-sample-translation-id-index
+create index sample_translation_id_idx on sample_translation(id);
