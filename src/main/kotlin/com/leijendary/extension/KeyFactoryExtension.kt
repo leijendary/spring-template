c2514277803a3af74f652a362e805136b6b5b439
leijendary <jonathanleijendekker@gmail.com>
2024-06-14 09:47:35 +0200

    [no ci] Removed global variable for base64 decoder

--- src/main/kotlin/com/leijendary/extension/KeyFactoryExtension.kt
+++ src/main/kotlin/com/leijendary/extension/KeyFactoryExtension.kt
@@ -7,18 +7,16 @@ import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
 import java.util.*
 
-private val DECODER = Base64.getDecoder()
-
 fun KeyFactory.rsaPrivateKey(privateKey: String): RSAPrivateKey {
-    val base64 = DECODER.decode(privateKey)
-    val keySpec = PKCS8EncodedKeySpec(base64)
+    val decoded = Base64.getDecoder().decode(privateKey)
+    val keySpec = PKCS8EncodedKeySpec(decoded)
 
     return generatePrivate(keySpec) as RSAPrivateKey
 }
 
 fun KeyFactory.rsaPublicKey(publicKey: String): RSAPublicKey {
-    val base64 = DECODER.decode(publicKey)
-    val keySpec = X509EncodedKeySpec(base64)
+    val decoded = Base64.getDecoder().decode(publicKey)
+    val keySpec = X509EncodedKeySpec(decoded)
 
     return generatePublic(keySpec) as RSAPublicKey
 }
