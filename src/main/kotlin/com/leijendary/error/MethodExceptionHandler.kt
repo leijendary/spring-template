dc8569cd27cb9563b59506653d31838ab5df6876
leijendary <jonathanleijendekker@gmail.com>
2024-08-02 09:43:04 +0200

    [no ci] changed code for MethodArgumentTypeMismatchException

--- src/main/kotlin/com/leijendary/error/MethodExceptionHandler.kt
+++ src/main/kotlin/com/leijendary/error/MethodExceptionHandler.kt
@@ -6,7 +6,6 @@ import com.leijendary.model.ErrorSource
 import org.springframework.context.MessageSource
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus.BAD_REQUEST
-import org.springframework.util.ClassUtils
 import org.springframework.validation.BindException
 import org.springframework.validation.FieldError
 import org.springframework.validation.ObjectError
@@ -49,16 +48,10 @@ class MethodExceptionHandler(private val messageSource: MessageSource) {
     @ExceptionHandler(MethodArgumentTypeMismatchException::class)
     @ResponseStatus(BAD_REQUEST)
     fun catchMethodArgumentTypeMismatch(exception: MethodArgumentTypeMismatchException): List<ErrorModel> {
-        val code = "error.invalid.type"
         val name = exception.name
-        val parameter = exception.parameter
-        val requiredType = parameter.parameterType.simpleName
-        val value = exception.value
-        val valueType = ClassUtils.getDescriptiveType(value)?.let(ClassUtils::getShortName)
-        val arguments = arrayOf(name, value, valueType, requiredType)
-        val message = messageSource.getMessage(code, arguments, requestContext.locale)
+        val message = messageSource.getMessage(CODE_BINDING_INVALID_VALUE, arrayOf(), requestContext.locale)
         val source = ErrorSource(pointer = "/param/$name")
-        val error = ErrorModel(code = code, message = message, source = source)
+        val error = ErrorModel(code = CODE_BINDING_INVALID_VALUE, message = message, source = source)
 
         return listOf(error)
     }
