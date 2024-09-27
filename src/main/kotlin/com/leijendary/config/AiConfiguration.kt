dc26d7929a1752f6ffc130b7e07d73396f7781f6
leijendary <jonathanleijendekker@gmail.com>
2024-09-27 09:16:30 +0200

    [no ci] removed variable for advisors

--- src/main/kotlin/com/leijendary/config/AiConfiguration.kt
+++ src/main/kotlin/com/leijendary/config/AiConfiguration.kt
@@ -3,7 +3,6 @@ package com.leijendary.config
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor
 import org.springframework.ai.chat.memory.ChatMemory
 import org.springframework.ai.vectorstore.VectorStore
 import org.springframework.beans.factory.annotation.Value
@@ -21,15 +20,12 @@ class AiConfiguration {
 
     @Bean
     fun chatClient(builder: ChatClient.Builder, vectorStore: VectorStore, chatMemory: ChatMemory): ChatClient {
-        val advisors = listOf(
-            PromptChatMemoryAdvisor(chatMemory),
-            QuestionAnswerAdvisor(vectorStore),
-            SimpleLoggerAdvisor()
-        )
-
         return builder
             .defaultSystem(generalInstructionSystem)
-            .defaultAdvisors(advisors)
+            .defaultAdvisors(
+                PromptChatMemoryAdvisor(chatMemory),
+                QuestionAnswerAdvisor(vectorStore),
+            )
             .build()
     }
 
