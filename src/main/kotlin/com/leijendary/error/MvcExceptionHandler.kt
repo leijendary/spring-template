e7b56ea243d4c35f40d9da4a7a619d7f17d9fb4a
leijendary <jonathanleijendekker@gmail.com>
2025-05-17 19:01:09 +0200

    Removed custom binding exception handler
    Signed-off-by: leijendary <jonathanleijendekker@gmail.com>

--- src/main/kotlin/com/leijendary/error/MvcExceptionHandler.kt
+++ src/main/kotlin/com/leijendary/error/MvcExceptionHandler.kt
@@ -1,63 +1,31 @@
 package com.leijendary.error
 
-import com.leijendary.context.RequestContext.isFragment
-import com.leijendary.extension.toErrorModels
-import com.leijendary.model.toMap
-import org.springframework.context.MessageSource
+import com.leijendary.context.RequestContext.currentRequest
+import com.leijendary.context.RequestContext.isApi
 import org.springframework.core.annotation.Order
-import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.method.HandlerMethod
-import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.servlet.resource.NoResourceFoundException
+import java.net.URI
 
 @ControllerAdvice
 @Order(2)
-class MvcExceptionHandler(private val messageSource: MessageSource) {
+class MvcExceptionHandler {
     @ExceptionHandler(NoResourceFoundException::class)
-    fun handleNoResourceFound(): String {
-        return "404"
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleHandlerMethodValidation(
-        exception: MethodArgumentNotValidException,
-        handlerMethod: HandlerMethod
-    ): ModelAndView {
-        val errors = exception.bindingResult.allErrors.toErrorModels(messageSource).toMap()
-        val problemDetail = exception.body.apply { setProperty(PROPERTY_ERRORS, errors) }
-        val view = resolveView(handlerMethod)
-        val model = ModelAndView(view, exception.statusCode)
-        model.addObject("target", exception.bindingResult.target)
-        model.addObject(PROPERTY_PROBLEM_DETAIL, problemDetail)
-
-        return model
-    }
-
-    private fun resolveView(handlerMethod: HandlerMethod): String {
-        val classMapping = handlerMethod.method
-            .declaringClass
-            .getDeclaredAnnotation(RequestMapping::class.java)
-            ?: return "500"
-        val methodMapping = handlerMethod.getMethodAnnotation(RequestMapping::class.java) ?: return "500"
-        var path = if (isFragment) "fragment" else "page"
-        val classPath = classMapping.value.firstOrNull()
-        val methodPath = methodMapping.value.firstOrNull()
-
-        if (classPath !== null) {
-            path += "/$classPath"
+    fun handleNoResourceFound(exception: NoResourceFoundException): Any {
+        if (!isApi) {
+            return "404"
         }
 
-        if (methodPath !== null) {
-            path += "/$methodPath"
+        val problemDetail = exception.body.apply {
+            title = HttpStatus.NOT_FOUND.reasonPhrase
+            instance = currentRequest?.requestURI?.let(::URI)
         }
 
-        if (classPath === null && methodPath === null) {
-            path += "/index"
-        }
-
-        return path
+        return ResponseEntity
+            .status(exception.statusCode)
+            .body(problemDetail)
     }
 }
